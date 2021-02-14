b16b0d04d78136978b763b5593c89faa5bc7b7b1
Prakash Kaliyannan <prakash.kaliyannan@cognizant.com>
2021-02-14 22:31:25 +0530

    angular version changed to 11 from 6

--- hms-ui/src/app/app.module.ts
+++ hms-ui/src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
