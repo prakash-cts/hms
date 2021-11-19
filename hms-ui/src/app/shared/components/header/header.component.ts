b028ddb74b3bd69189191e11a7a59b46a825f2dd
Prakash Kaliyannan <prakash.kaliyannan@cognizant.com>
2021-11-19 23:02:54 +0530

    v1.1 - adding login and logout functionality

--- hms-ui/src/app/shared/components/header/header.component.ts
+++ hms-ui/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,12 +11,17 @@ export class HeaderComponent implements OnInit {
 
 @Output() toggleSideBarForMe: EventEmitter<any> = new EventEmitter()
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
   }
   toggleSideBar(){
     this.toggleSideBarForMe.emit();
+  }
+  logout(){
+    this.router.navigate(['login']);
   }
 
 }
